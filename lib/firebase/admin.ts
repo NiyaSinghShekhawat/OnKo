8cc1b843a11a5a0f1d349df8df2615c451d0fc54
Shreyan2745 <dinosaur.20075@gmail.com>
2026-09-20 04:30:38 +0530

    fix(storage): expose Firebase Admin Storage accessor

--- lib/firebase/admin.ts
+++ lib/firebase/admin.ts
@@ -1,6 +1,7 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from "firebase-admin/storage";
 
 function adminApp() {
   if (getApps().length) return getApps()[0];
@@ -18,3 +19,4 @@ function adminApp() {
 }
 export const getAdminAuth = () => getAuth(adminApp());
 export const getAdminDb = () => getFirestore(adminApp());
+export const getAdminStorage = () => getStorage(adminApp());
