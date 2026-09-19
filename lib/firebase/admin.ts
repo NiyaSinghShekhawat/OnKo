b27d1020adecb498709faa88533976ffc853f2fa
Shreyan2745 <dinosaur.20075@gmail.com>
2026-09-20 03:19:47 +0530

    chore(firebase): remove storage dependency for free prototype

--- lib/firebase/admin.ts
+++ lib/firebase/admin.ts
@@ -1,7 +1,6 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
-import { getStorage } from "firebase-admin/storage";
 
 function adminApp() {
   if (getApps().length) return getApps()[0];
@@ -15,9 +14,7 @@ function adminApp() {
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: key,
     }),
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET || process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   });
 }
 export const getAdminAuth = () => getAuth(adminApp());
 export const getAdminDb = () => getFirestore(adminApp());
-export const getAdminStorage = () => getStorage(adminApp());
